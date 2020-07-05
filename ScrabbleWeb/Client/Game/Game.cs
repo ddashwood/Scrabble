edf2a9d17d547770865a9c0058e68cfb92c516e7
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2020-07-05 09:24:24 +0100

    Refactoring to create a client Game class and stop using DTO in inappropriate places

---
+++ ScrabbleWeb/Client/Game/Game.cs
@@ -0,0 +1,32 @@
+﻿using ScrabbleMoveChecker;
+using ScrabbleWeb.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrabbleWeb.Client.Game
+{
+    public class Game : GameBase
+    {
+        public Game(GameDto dto)
+            :base(dto.Board)
+        {
+            GameId = dto.GameId;
+            PlayerTiles = dto.PlayerTiles;
+            Player1Score = dto.Player1Score;
+            Player2Score = dto.Player2Score;
+            IsPlayer1 = dto.IsPlayer1;
+            OtherPlayerName = dto.OtherPlayerName;
+            Move = new MoveBase(this);
+        }
+
+        public int GameId { get; set; }
+        public string PlayerTiles { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+        public bool IsPlayer1 { get; set; }
+        public string OtherPlayerName { get; set; }
+        public MoveBase Move { get; set; }
+    }
+}
