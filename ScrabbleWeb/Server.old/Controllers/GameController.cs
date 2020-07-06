22004ee8225ce8e39b30fb6d9eec4388acc1254c
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2020-07-06 11:20:53 +0100

    Scaffold identity

---
+++ ScrabbleWeb/Server.old/Controllers/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ScrabbleData;
+using ScrabbleGame;
+using ScrabbleMoveChecker;
+using ScrabbleWeb.Server.Data;
+using ScrabbleWeb.Shared;
+
+namespace ScrabbleWeb.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IWordCheckerFactory wordCheckerFactory;
+        public GameController(ApplicationDbContext context, IWordCheckerFactory wordCheckerFactory)
+        {
+            this.context = context;
+            this.wordCheckerFactory = wordCheckerFactory;
+            game = game ?? new Game(wordCheckerFactory.GetWordChecker());
+        }
+
+        static Game game;
+
+        [HttpGet("{id}")]
+        public GameDto Get(int id)
+        {
+            string board = game.Board;
+            return new GameDto
+            {
+                Board = board,
+                PlayerTiles = game.Player1Tiles,
+                OtherPlayerName = "Test Player"
+            };
+        }
+
+        [HttpPost("{id}")]
+        public ActionResult<MoveResultDto> Post(List<TilePlacement> placements, int id)
+        {
+            var move = new Move(game);
+            foreach (var placement in placements)
+            {
+                move.AddPlacement(placement);
+            }
+
+            int score = move.GetScore(out string error);
+            if (!string.IsNullOrEmpty(error))
+            {
+                return UnprocessableEntity(new MoveResultDto(error));
+            }
+
+            var badWords = move.InvalidWords().ToList();
+
+            if (badWords.Count() != 0)
+            {
+                var result = new MoveResultDto("The following words are not allowed: " + string.Join(", ", badWords));
+                result.InvalidWords = badWords;
+                return UnprocessableEntity(result);
+            }
+
+            move.Play();
+            return Ok(new MoveResultDto(Get(id)));
+        }
+    }
+}
---
+++ ScrabbleWeb/Server.old/Data/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using ScrabbleWeb.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScrabbleData;
+using Microsoft.AspNetCore.Identity;
+using IdentityServer4.Models;
+using ScrabbleGame;
+
+namespace ScrabbleWeb.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<Player>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<GameData> Games { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Change default table names to singular
+            builder.Entity<Player>(entity => { entity.ToTable(name: "Player"); });
+            builder.Entity<IdentityRole>(entity => { entity.ToTable(name: "Role"); });
+            builder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("UserRole"); });
+            builder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("UserClaim"); });
+            builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("UserLogin"); });
+            builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("UserToken"); });
+            builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("RoleClaim"); });
+
+            builder.Entity<GameData>(entity =>
+            {
+                entity
+                    .ToTable("Game")
+                    .HasKey(g => g.GameId);
+                entity
+                    .HasOne<Player>()
+                    .WithMany(p => p.GamesAsPlayer1)
+                    .HasForeignKey("Player1Id")
+                    .HasPrincipalKey("Id");
+                entity
+                    .HasOne<Player>()
+                    .WithMany(p => p.GamesAsPlayer2)
+                    .HasForeignKey("Player2Id")
+                    .HasPrincipalKey("Id");
+
+                entity.HasCheckConstraint("CK_BOARD_LENGTH", $"LENGTH(Board)={Game.BOARD_WIDTH * Game.BOARD_HEIGHT}");
+            });
+        }
+    }
+
+}
