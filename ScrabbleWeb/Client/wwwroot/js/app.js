47083063574fc37dfa015ec7145f700fef9c165a
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2020-07-08 17:05:03 +0100

    Prevent screen from scrolling when a tile is played

---
+++ ScrabbleWeb/Client/wwwroot/js/app.js
@@ -0,0 +1,15 @@
+﻿var scrollPreventer = {
+    previousTop: 0,
+
+    saveTop: function (elId) {
+        var rect = document.getElementById(elId).getBoundingClientRect();
+        this.previousTop = rect.top;
+        console.log("Saved " + this.previousTop);
+    },
+    restoreTop: function (elId) {
+        var rect = document.getElementById(elId).getBoundingClientRect();
+        var currentTop = rect.top;
+        window.scrollBy(0, currentTop - this.previousTop);
+        console.log("Restored " + this.previousTop);
+    }
+}
